f7159d7439bb7b7655723166e7c71910c5b0497e
Eshaan0112 <eminocha@purdue.edu>
2023-02-17 22:20:21 -0500

    Diffoscoping

---
+++ difference.py
@@ -0,0 +1,16 @@
+# import diffoscope
+import diffoscope.comparators
+
+print('Diffoscoping')
+# path to the first repository
+repo1_path = ""
+
+# path to the second repository
+repo2_path = ""
+
+# generate a diffoscope report for the two repositories
+# diffoscope_report = diffoscope.main[(repo1_path, repo2_path)]
+diffoscope_report = diffoscope.comparators.compare(repo1_path, repo2_path)
+
+# print the diffoscope report
+print(diffoscope_report)
