c7c2584c904c37b483c9edbc9f8dcad19c8d94b1
Trevor Taka <trevorhktaka@gmail.com>
2023-03-05 19:06:47 -0500

    Add static table to home page
    Added table to contain currently owned stocks. In the future, it will
    update when we received currently owned stocks from json.
    Minor changes: Add stye.css file, make ts semicolon style consistent

--- FrontEnd/homePage.ts
+++ FrontEnd/homePage.ts
@@ -2,29 +2,29 @@ document.body.onload = initializeBalance;
 
 function initializeBalance()
 {
-    let balance = 25000;
-    const app = document.getElementById("balance")!;
+    let balance = 25000
+    const app = document.getElementById("balance")!
     const p = document.createElement("p")
-    p.innerHTML = "Balance: $"+balance;
+    p.innerHTML = "Balance: $"+balance
     app.append(p)
 }
 
 function runBot() {
     console.log('Typescript hit')
-    const input = document.getElementById("stock_input") as HTMLSelectElement;
+    const input = document.getElementById("stock_input") as HTMLSelectElement
     console.log('Input')
-    const selectedStrings = Array.from(input.selectedOptions, option => option.value);
+    const selectedStrings = Array.from(input.selectedOptions, option => option.value)
     console.log(selectedStrings)
   
     if (selectedStrings.length === 0) {
-      console.error("No strings selected.");
+      console.error("No strings selected.")
       return;
     }
   
     const formData = new FormData();
     for (let i = 0; i < selectedStrings.length; i++) {
       const string = selectedStrings[i]
-      formData.append("strings[]", string);
+      formData.append("strings[]", string)
     }
     const local_hostUrl = `http://localhost:5000/upload`;
   
@@ -36,11 +36,11 @@ function runBot() {
     .then(json => {
         console.log('Displaying on webpage - directory path: ', json.path)
 
-        const balanceArea = document.getElementById("balance")!;
-        balanceArea.innerHTML = "Balance: $"+(json.JsonList.total_capital).toFixed(2);
+        const balanceArea = document.getElementById("balance")!
+        balanceArea.innerHTML = "Balance: $"+(json.JsonList.total_capital).toFixed(2)
 
       })
       .catch(error => {
-        console.error(error);
-      });
+        console.error(error)
+      })
   }
