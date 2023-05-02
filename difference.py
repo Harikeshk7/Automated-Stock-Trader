4e8d677acda5099b22d676686311830e87618fe2
Eshaan0112 <eminocha@purdue.edu>
2023-05-02 18:30:48 -0400

    Checking dependencies in CI

--- difference.py
+++ difference.py
@@ -1,15 +1,15 @@
-# import diffoscope
+import diffoscope
 import diffoscope.comparators
 
 print('Diffoscoping')
 # path to the first repository
-repo1_path = ""
+# repo1_path = ""
 
-# path to the second repository
-repo2_path = ""
+# # path to the second repository
+# repo2_path = ""
 
-# generate a diffoscope report for the two repositories
-diffoscope_report = diffoscope.comparators.compare(repo1_path, repo2_path)
+# # generate a diffoscope report for the two repositories
+# diffoscope_report = diffoscope.comparators.compare(repo1_path, repo2_path)
 
-# print the diffoscope report
-print(diffoscope_report)
+# # print the diffoscope report
+# print(diffoscope_report)
